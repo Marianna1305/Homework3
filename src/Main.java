bcc0eb66e76cf25c0edc6363a44439489aee63eb
makeeva_m <svobinteva@yandex.ru>
2022-10-27 16:14:53 +0300

    Условия ДЗ 3

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+// Объявите переменные типа int, byte, short, long, float, double.
+// Выведите в консоль значение каждой переменной в формате "Значение переменной … с типом … равно …"
+        System.out.println("Hello world!");
+    }
+}
